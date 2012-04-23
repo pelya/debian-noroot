2b64bff904d5cc8d2aeaa9764c1a6bc115bb8c25
pelya <x.pelya.x@gmail.com>
2012-04-23 21:03:54 +0300

    Updated build script

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+fail() { echo "Compilation failed!" ; exit 1; }
+
 cd fakechroot
-[ -e fakechroot_2.16.orig.tar.gz ] || git archive --format=tar HEAD | gzip > ../fakechroot_2.16.orig.tar.gz
-dpkg-buildpackage -aarmel -us -uc
+[ -e configure ] || ./autogen.sh || fail
+[ -e config.h ] || ./configure --host=arm-linux-gnueabi --prefix=/usr || fail
+make -j4 && cp -f src/.libs/libfakechroot.so .. && arm-linux-gnueabi-strip -g ../libfakechroot.so || fail
+cd ..
+
+cd xserver
+
