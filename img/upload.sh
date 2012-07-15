90601b2938131c56b06fadef50d806078bded528
pelya <x.pelya.x@gmail.com>
2012-07-15 22:14:46 +0300

    Small fix to upload script

--- img/upload.sh
+++ img/upload.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rsync -e ssh --progress -h --timeout 10 -a -v dist-gimp-precise.zip "pelya@frs.sourceforge.net:/home/frs/project/l/li/libsdl-android/ubuntu/"
+rsync -e ssh --progress -h --timeout 10 -a -v *.zip "pelya@frs.sourceforge.net:/home/frs/project/l/li/libsdl-android/ubuntu/"
