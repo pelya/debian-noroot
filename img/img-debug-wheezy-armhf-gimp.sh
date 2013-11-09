9ac4a9e033ced5423a41ca6ea24e70f5e5b73c33
pelya <x.pelya.x@gmail.com>
2013-11-09 23:36:27 +0200

    Some fixes to Gimp image

--- img/img-debug-wheezy-armhf-gimp.sh
+++ img/img-debug-wheezy-armhf-gimp.sh
@@ -7,7 +7,7 @@ APT_CACHER=
 [ -e /etc/init.d/apt-cacher ] && APT_CACHER=/localhost:3142
 sudo qemu-debootstrap --arch=armhf --verbose \
         --components=main,universe,restricted,multiverse \
-        --include=fakeroot,fakechroot,xfonts-base,xfce4-panel,xfdesktop4,metacity,gimp,inkscape \
+        --include=fakeroot,fakechroot,xfonts-base,x11-xserver-utils,xfce4-panel,xfdesktop4,xfwm4,xfce4-session,thunar,xfconf,xfce4-settings,gimp,inkscape \
         wheezy $DIR http:/$APT_CACHER/ftp.ua.debian.org/debian/ \
 && cat sources-jessie.list | sed 's/jessie/wheezy/g' | sudo tee $DIR/etc/apt/sources.list > /dev/null \
 && sudo cp -a $DIR $DIR-back && sudo ./prepare-img.sh $DIR /data/local/tmp/img
