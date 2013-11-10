f5de154cf27810a15d2f6314a728fd5989afc5e0
pelya <x.pelya.x@gmail.com>
2013-11-10 19:27:44 +0200

    Added logs to postinstall

--- dist/postinstall.sh
+++ dist/postinstall.sh
@@ -1,28 +1,31 @@
 #!/system/bin/sh
 
 case x$SDCARD in x ) export SDCARD=$EXTERNAL_STORAGE;; esac
+echo "SDCARD path: $SDCARD"
+echo "Changing curdir to: $SECURE_STORAGE_DIR"
+case x$SECURE_STORAGE_DIR in x ) echo ... > /dev/null;; * ) cd $SECURE_STORAGE_DIR;; esac
 
-case x$SECURE_STORAGE_DIR in x ) echo ...;; * ) cd $SECURE_STORAGE_DIR;; esac
+ln -sf $SDCARD sdcard
 
-ln -s $SDCARD sdcard
-
+echo "Creating necessary directories"
 # Random post-install cmds
-mkdir var/run/dbus
-mkdir var/run/xauth
-ln -s `pwd`/usr/bin/dbus-launch `pwd`/bin/dbus-launch
-mkdir var/lib/dbus
-mkdir root
-mkdir root/Desktop
-chmod 644 root/Desktop/*
-ln -s $SDCARD root/sdcard
-ln -s $SDCARD root/Desktop/sdcard
+mkdir -p var/run/dbus
+mkdir -p var/run/xauth
+ln -sf `pwd`/usr/bin/dbus-launch `pwd`/bin/dbus-launch
+mkdir -p var/lib/dbus
+mkdir -p root
+mkdir -p root/Desktop
+ln -sf $SDCARD root/sdcard
+ln -sf $SDCARD root/Desktop/sdcard
 
+echo "Updating lib paths"
 ./updatelibpaths.sh > libpaths
 
 ls lib/ld-linux-armel.so.3 && ln -s ld-linux-armhf.so.3 lib/ld-linux.so.3
 ls lib/ld-linux-armhf.so.3 && ln -s ld-linux-armhf.so.3 lib/ld-linux.so.3
 
-#./chroot.sh usr/sbin/groupadd -g $USER_ID $USER
-#./chroot.sh usr/sbin/useradd -g $USER_ID -G sudo,staff -m $USER
-./chroot.sh usr/sbin/useradd -U -m -G sudo,staff -p '$1$nFL/I4tz$zHKmBfkaKmRRmWje1Mupm0' -u $USER_ID $USER
-./chroot.sh bin/cp -a -f root home/$USER
+echo "Adding user $USER ID $USER_ID"
+./chroot.sh usr/sbin/useradd -U -m -G sudo,staff -p '$1$nFL/I4tz$zHKmBfkaKmRRmWje1Mupm0' -u $USER_ID $USER 2>&1
+./chroot.sh bin/cp -a -f root home/$USER 2>&1
+
+echo "Postinstall script finished"
