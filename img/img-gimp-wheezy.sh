b5d1fe56682f5d83c1017a2eaaa3adbd767fa3ca
pelya <x.pelya.x@gmail.com>
2013-11-10 23:01:36 +0200

    Icon theme was missing

--- img/img-gimp-wheezy.sh
+++ img/img-gimp-wheezy.sh
@@ -8,7 +8,7 @@ APT_CACHER=
 [ -e /etc/init.d/apt-cacher ] && APT_CACHER=/localhost:3142
 sudo qemu-debootstrap --arch=armhf --verbose \
         --components=main,universe,restricted,multiverse \
-        --include=fakeroot,fakechroot,xfonts-base,x11-xserver-utils,xfce4-panel,xfdesktop4,xfwm4,xfce4-session,thunar,xfconf,xfce4-settings,gimp,inkscape \
+        --include=fakeroot,fakechroot,xfonts-base,x11-xserver-utils,xfce4-panel,xfdesktop4,xfwm4,xfce4-session,thunar,xfconf,xfce4-settings,tango-icon-theme,gimp,inkscape \
         wheezy $DIR http:/$APT_CACHER/ftp.ua.debian.org/debian/ \
 && cat sources-jessie.list | sed 's/jessie/wheezy/g' | sudo tee $DIR/etc/apt/sources.list > /dev/null \
 && sudo ./prepare-img.sh $DIR $DEST
