200fd496204f1bd69190de5d3f52c82e379eea3e
Sergii Pylypenko <x.pelya.x@gmail.com>
2013-11-11 17:22:59 +0200

    libfakedns: do not export C-Ares stuff

--- build.sh
+++ build.sh
@@ -5,11 +5,29 @@ fail() { echo "Compilation failed!" ; exit 1; }
 cd fakechroot
 [ -e configure ] || ./autogen.sh || fail
 [ -e config.h ] || ./configure --host=arm-linux-gnueabihf --prefix=/usr || fail
-[ -e ../libfakechroot.so ] || { make -j4 CFLAGS="-march=armv7-a" LDFLAGS="-march=armv7-a" V=1 && cp -f src/.libs/libfakechroot.so .. && arm-linux-gnueabihf-strip ../libfakechroot.so || fail ; }
+[ -e ../libfakechroot.so ] || {
+	make -j4 CFLAGS="-march=armv7-a" LDFLAGS="-march=armv7-a" V=1 && \
+	cp -f src/.libs/libfakechroot.so .. && \
+	arm-linux-gnueabihf-strip ../libfakechroot.so || fail
+}
 cd ..
 
 cd c-ares
-[ -e ares_config.h ] || ./configure --enable-shared --host=arm-linux-gnueabihf --prefix=/usr || fail
-[ -e ../libfakedns.so ] || { make -j4 CFLAGS="-march=armv7-a" LDFLAGS="-march=armv7-a" && cp -f .libs/libcares.so ../libfakedns.so && arm-linux-gnueabihf-strip ../libfakedns.so || fail ; }
+[ -e ares_config.h ] || LIBS=-Wl,--version-script=exports.txt \
+	./configure --enable-shared --host=arm-linux-gnueabihf --prefix=/usr || fail
+[ -e ../libfakedns.so ] || {
+	make -j4 CFLAGS="-march=armv7-a" LDFLAGS="-march=armv7-a" .libs/libcares.so && \
+	cp -f .libs/libcares.so ../libfakedns.so && \
+	arm-linux-gnueabihf-strip ../libfakedns.so || fail
+}
 cd ..
 
+[ -e libfakedns.so ] || {
+	arm-linux-gnueabihf-gcc -march=armv7-a -shared fakedns/*.c -I c-ares c-ares/.libs/libcares.a -o libfakedns.so && \
+	arm-linux-gnueabihf-strip libfakedns.so || fail
+}
+
+[ -e libdisableselinux.so ] || {
+	arm-linux-gnueabihf-gcc -march=armv7-a -shared disableselinux/*.c -o libdisableselinux.so && \
+	arm-linux-gnueabihf-strip libdisableselinux.so || fail
+}
