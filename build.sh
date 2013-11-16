d951a5191a962805f656d3a7723e74a9f57b977d
pelya <x.pelya.x@gmail.com>
2013-11-16 19:43:05 +0200

    Added andorid-shmem, added -fpic everywhere

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ cd fakechroot
 [ -e configure ] || ./autogen.sh || fail
 [ -e config.h ] || ./configure --host=arm-linux-gnueabihf --prefix=/usr || fail
 [ -e ../libfakechroot.so ] || {
-	make -j4 CFLAGS="-march=armv7-a" LDFLAGS="-march=armv7-a" V=1 && \
+	make -j4 CFLAGS="-march=armv7-a -fpic" LDFLAGS="-march=armv7-a" V=1 && \
 	cp -f src/.libs/libfakechroot.so .. && \
 	arm-linux-gnueabihf-strip ../libfakechroot.so || fail
 }
@@ -16,18 +16,37 @@ cd c-ares
 [ -e ares_config.h ] || LIBS=-Wl,--version-script=exports.txt \
 	./configure --enable-shared --host=arm-linux-gnueabihf --prefix=/usr || fail
 [ -e ../libfakedns.so ] || {
-	make -j4 CFLAGS="-march=armv7-a" LDFLAGS="-march=armv7-a" libcares.la && \
+	make -j4 CFLAGS="-march=armv7-a -fpic" LDFLAGS="-march=armv7-a" libcares.la && \
 	cp -f .libs/libcares.so ../libfakedns.so && \
 	arm-linux-gnueabihf-strip ../libfakedns.so || fail
 }
 cd ..
 
 [ -e libfakedns.so ] || {
-	arm-linux-gnueabihf-gcc -march=armv7-a -shared fakedns/*.c -I c-ares c-ares/.libs/libcares.a -o libfakedns.so && \
+	arm-linux-gnueabihf-gcc -march=armv7-a -shared -fpic fakedns/*.c -I c-ares c-ares/.libs/libcares.a -o libfakedns.so && \
 	arm-linux-gnueabihf-strip libfakedns.so || fail
 }
 
 [ -e libdisableselinux.so ] || {
-	arm-linux-gnueabihf-gcc -march=armv7-a -shared disableselinux/*.c -o libdisableselinux.so && \
+	arm-linux-gnueabihf-gcc -march=armv7-a -shared -fpic disableselinux/*.c -o libdisableselinux.so && \
 	arm-linux-gnueabihf-strip libdisableselinux.so || fail
 }
+
+[ -e libandroid-shmem.so ] || {
+	[ -e android-shmem/LICENSE ] || {
+		cd ..
+		git submodule update --init android/android-shmem || fail
+		cd $BUILDDIR
+	} || exit 1
+	[ -e android-shmem/libancillary/ancillary.h ] || {
+		cd android-shmem
+		git submodule update --init libancillary || fail
+		cd ..
+	} || exit 1
+
+	cd android-shmem
+	arm-linux-gnueabihf-gcc -march=armv7-a -shared -fpic -std=gnu99 *.c -I . -I libancillary \
+		-o ../libandroid-shmem.so -Wl,--version-script=exports.txt -lc -lpthread && \
+	arm-linux-gnueabihf-strip ../libandroid-shmem.so || fail
+	cd ..
+} || exit 1
