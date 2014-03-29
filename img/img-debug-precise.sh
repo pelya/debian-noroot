e365f593cba506f843edbc6e582ac91e80e51059
pelya <x.pelya.x@gmail.com>
2014-03-29 23:24:12 +0200

    Cleaning up rootfs generation scripts

--- img/img-debug-precise.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-echo "Ubuntu Natty is the last Ubuntu supporting 2.6.X kernel - Precise will not work on many devices, because it requires kernel 3.0.X"
-# libreoffice-calc,libreoffice-draw,libreoffice-impress,libreoffice-math,libreoffice-writer
-sudo rm -r -f dist-debug-precise
-sudo qemu-debootstrap --arch=armel --verbose \
-        --components=main,universe,restricted,multiverse \
-        --include=ubuntu-minimal,fakeroot,fakechroot,xfonts-base,synaptic,socat,putty,xfce4-panel,xfce4-session,xfce4-utils,xfdesktop4,xfwm4,gimp,strace \
-        precise dist-debug-precise \
-&& sudo cp -a dist-debug-precise dist-debug-precise-back && sudo ./prepare-img.sh dist-debug-precise /data/local/ubuntu
--- img/img-gimp-wheezy-armhf.sh
+++ img/img-gimp-wheezy-armhf.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 echo "This image requires kernel 2.6.29, it will run on emulator"
 
-DIR=dist-gimp-wheezy
+DIR=dist-gimp-wheezy-armhf
 DEST=/data/data/org.gimp.inkscape/files
 sudo rm -r -f $DIR
 APT_CACHER=
 [ -e /etc/init.d/apt-cacher ] && APT_CACHER=/localhost:3142
 sudo qemu-debootstrap --arch=armhf --verbose \
         --components=main,universe,restricted,multiverse \
-        --include=fakeroot,fakechroot,xfonts-base,x11-xserver-utils,xfce4-panel,xfdesktop4,xfwm4,xfce4-session,thunar,xfconf,xfce4-settings,tango-icon-theme,shiki-colors-xfwm-theme,gimp,inkscape \
+        --include=`cat img-gimp-wheezy.pkg` \
         wheezy $DIR http:/$APT_CACHER/ftp.ua.debian.org/debian/ \
 && cat sources-jessie.list | sed 's/jessie/wheezy/g' | sudo tee $DIR/etc/apt/sources.list > /dev/null \
 && sudo ./prepare-img.sh $DIR $DEST
