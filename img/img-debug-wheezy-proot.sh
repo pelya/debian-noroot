83814520bdcc7de780dc2e83c00c714f59bb7063
Sergii Pylypenko <x.pelya.x@gmail.com>
2014-03-31 23:07:41 +0300

    Forgot to add Xterm to image generation script

--- img/img-debug-wheezy-proot.sh
+++ img/img-debug-wheezy-proot.sh
@@ -7,7 +7,7 @@ APT_CACHER=
 [ -e /etc/init.d/apt-cacher ] && APT_CACHER=/localhost:3142
 sudo qemu-debootstrap --arch=armhf --verbose \
         --components=main,universe,restricted,multiverse \
-        --include=fakeroot,fakechroot,xfonts-base,x11-xserver-utils,xfce4-panel,xfdesktop4,xfwm4,xfce4-session,thunar,xfconf,xfce4-settings,tango-icon-theme,apt,synaptic,apt-xapian-index \
+        --include=fakeroot,fakechroot,xfonts-base,x11-xserver-utils,xfce4-panel,xfdesktop4,xfwm4,xfce4-session,thunar,xfconf,xfce4-settings,tango-icon-theme,apt,synaptic,apt-xapian-index,xterm \
         wheezy $DIR http:/$APT_CACHER/ftp.ua.debian.org/debian/ \
 && cat sources-jessie.list | sed 's/jessie/wheezy/g' | sudo tee $DIR/etc/apt/sources.list > /dev/null \
 && sudo ./prepare-img-proot.sh $DIR
