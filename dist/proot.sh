e614113f7be3a71e82274f66b99895e3d2ce0892
Sergii Pylypenko <x.pelya.x@gmail.com>
2014-03-31 22:56:37 +0300

    Updated proot script

--- dist/proot.sh
+++ dist/proot.sh
@@ -1,5 +1,7 @@
 #!/system/bin/sh
 
+case x$SECURE_STORAGE_DIR in x ) echo ... > /dev/null;; * ) cd $SECURE_STORAGE_DIR/img;; esac
+
 case x$SDCARD in x ) export SDCARD=$EXTERNAL_STORAGE;; esac
 
 SDCARD=`./busybox realpath $SDCARD`
@@ -11,4 +13,4 @@ export SHELL=/bin/bash
 export LD_LIBRARY_PATH=
 export PATH=/usr/local/bin:/usr/local/sbin:/usr/sbin:/sbin:/usr/bin:/bin:/usr/local/games:/usr/games
 export "LD_PRELOAD=/libdisableselinux.so /libandroid-shmem.so"
-./proot -r `pwd` -w / -b /dev -b /proc -b /sys -b /system -b $SDCARD bin/bash -l
+./proot -r `pwd` -w / -b /dev -b /proc -b /sys -b /system -b $SDCARD bin/sh -l "$@"
