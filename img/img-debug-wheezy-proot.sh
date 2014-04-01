28fcc96f7c269f57d297b20de65926e5ef75defc
Sergii Pylypenko <x.pelya.x@gmail.com>
2014-04-01 23:14:47 +0300

    Added locales to the default packages, this increased package size considerably

--- img/img-debug-wheezy-proot.sh
+++ img/img-debug-wheezy-proot.sh
@@ -6,6 +6,6 @@ APT_CACHER=
 [ -e /etc/init.d/apt-cacher ] && APT_CACHER=/localhost:3142
 sudo qemu-debootstrap --arch=armhf --verbose \
         --components=main,universe,restricted,multiverse \
-        --include=fakeroot,fakechroot,libc-bin,dpkg,xfonts-base,x11-xserver-utils,xfce4-panel,xfdesktop4,xfwm4,xfce4-session,thunar,xfconf,xfce4-settings,tango-icon-theme,apt,synaptic,apt-xapian-index,xterm \
+        --include=fakeroot,fakechroot,libc-bin,dpkg,xfonts-base,x11-xserver-utils,xfce4-panel,xfdesktop4,xfwm4,xfce4-session,thunar,xfconf,xfce4-settings,tango-icon-theme,apt,synaptic,apt-xapian-index,xterm,locales \
         wheezy $DIR http:/$APT_CACHER/ftp.ua.debian.org/debian/ \
 && sudo ./prepare-img-proot.sh $DIR
