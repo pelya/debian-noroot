60ed2e6438a873a9f8771a286f987212e494f5cf
Sergii Pylypenko <x.pelya.x@gmail.com>
2014-05-13 16:41:37 +0300

    Fixed Java failing to run

--- dist/proot.sh
+++ dist/proot.sh
@@ -9,6 +9,7 @@ SDCARD=`./busybox realpath $SDCARD`
 export HOME=/home/$USER
 export SHELL=/bin/bash
 export LD_LIBRARY_PATH=
-export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+# Java doesn't work in PRoot when started from /usr/bin/java symlink, so we have to put a path to java binary into PATH
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/lib/jvm/default-java/bin:/usr/sbin:/usr/bin:/sbin:/bin
 export "LD_PRELOAD=/libdisableselinux.so /libandroid-shmem.so"
 ./proot -r `pwd` -w / -b /dev -b /proc -b /sys -b /system -b $SDCARD "$@"
