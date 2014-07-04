83e477f8e63b4ec09e40ca53470413dd75bfc33c
Sergii Pylypenko <x.pelya.x@gmail.com>
2014-07-04 23:09:52 +0300

    Fixed a bug in Java path resolving

--- dist/proot.sh
+++ dist/proot.sh
@@ -12,7 +12,7 @@ export SHELL=/bin/bash
 export LD_LIBRARY_PATH=
 # Java doesn't work in PRoot when started from /usr/bin/java symlink, so we have to put a path to java binary into PATH, and Java 6 fails on Samsung devices
 JAVA_PATH=/usr/lib/jvm/default-java/jre/bin:/usr/lib/jvm/default-java/bin
-ls usr/lib/jvm/java-7-openjdk-* > /dev/null 2>&1 && JAVA_PATH=/usr/lib/jvm/java-7-openjdk-*/jre/bin:/usr/lib/jvm/java-7-openjdk-*/bin
+ls usr/lib/jvm/java-7-openjdk-*/bin > /dev/null 2>&1 && JAVA_PATH=/`echo usr/lib/jvm/java-7-openjdk-*/jre/bin`:/`echo usr/lib/jvm/java-7-openjdk-*/bin`
 export PATH=/usr/local/sbin:/usr/local/bin:$JAVA_PATH:/usr/sbin:/usr/bin:/sbin:/bin
 export "LD_PRELOAD=/libdisableselinux.so /libandroid-shmem.so"
 ./proot -r `pwd` -w / -b /dev -b /proc -b /sys -b /system -b $SDCARD -b $UNSECURE_STORAGE_DIR "$@"
