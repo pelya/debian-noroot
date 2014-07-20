30cc44698a7217a5fb027c45d975b02199281352
Sergii Pylypenko <x.pelya.x@gmail.com>
2014-07-20 21:54:52 +0300

    Updated CUPS image

--- img/img-cups-wheezy.sh
+++ img/img-cups-wheezy.sh
@@ -3,7 +3,7 @@
 set -x
 
 APT_CACHER=
-[ -e /etc/init.d/apt-cacher ] && APT_CACHER=/localhost:3142
+[ -e /etc/init.d/apt-cacher* ] && APT_CACHER=/localhost:3142
 
 DIST=wheezy
 rm -f debootstrap.log
@@ -18,9 +18,8 @@ STRIP_LIST=`cat strip.list`
 STRIP_LIST=`echo $STRIP_LIST | sed 's/ /,/g'`
 sudo qemu-debootstrap --arch=$ARCH_DEBIAN --verbose \
 		--components=main,contrib,non-free \
-		--include=fakeroot,libc-bin,cups,apt,smbclient \
-		--exclude=$STRIP_LIST \
-		$DIST $DIR http:/$APT_CACHER/ftp.ua.debian.org/debian/ 2>&1 | tee -a debootstrap.log \
+		--include=fakeroot,libc-bin,cups,apt,smbclient,printer-driver-all-enforce,foomatic-db-compressed-ppds \
+		$DIST $DIR http:/$APT_CACHER/ftp.ua.debian.org/debian/ 2>&1 | tee -a debootstrap-$ARCH_ANDROID.log \
 && cat sources-jessie.list | sed "s/jessie/$DIST/g" | sudo tee $DIR/etc/apt/sources.list > /dev/null \
 && sudo ./prepare-img-proot.sh --strip "usr/share/X11 usr/share/zoneinfo usr/share/calendar" --noarchive $DIR $ARCH_ANDROID
 done
