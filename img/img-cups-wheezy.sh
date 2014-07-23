5d579340cb2a7173985575f123f165f318a41217
pelya <x.pelya.x@gmail.com>
2014-07-23 22:56:17 +0300

    Cups: forgot cups-client, doh

--- img/img-cups-wheezy.sh
+++ img/img-cups-wheezy.sh
@@ -18,7 +18,7 @@ STRIP_LIST=`cat strip.list`
 STRIP_LIST=`echo $STRIP_LIST | sed 's/ /,/g'`
 sudo qemu-debootstrap --arch=$ARCH_DEBIAN --verbose \
 		--components=main,contrib,non-free \
-		--include=fakeroot,libc-bin,cups,apt,smbclient,printer-driver-all-enforce,foomatic-db-compressed-ppds \
+		--include=cups,cups-client,smbclient,printer-driver-all-enforce,foomatic-db-compressed-ppds \
 		$DIST $DIR http:/$APT_CACHER/ftp.ua.debian.org/debian/ 2>&1 | tee -a debootstrap-$ARCH_ANDROID.log \
 && cat sources-jessie.list | sed "s/jessie/$DIST/g" | sudo tee $DIR/etc/apt/sources.list > /dev/null \
 && sudo ./prepare-img-proot.sh --strip "usr/share/X11 usr/share/zoneinfo usr/share/calendar" --noarchive $DIR $ARCH_ANDROID
