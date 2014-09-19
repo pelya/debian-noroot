e59d45cb9604d54f87a7ed8f0beff52c7927ba5e
Sergii Pylypenko <x.pelya.x@gmail.com>
2014-09-19 20:45:11 +0300

    XFCE logout will kill the app

--- dist/start-gimp.sh
+++ dist/start-gimp.sh
@@ -1,18 +1,12 @@
 #!/bin/sh
 
-#export LANG=C
-#export LANGUAGE=C
-
 rm -f /var/run/dbus/pid
 dbus-daemon --system &
 sleep 1
-{
-	dbus-launch --exit-with-session xfce4-session || {
-		xfwm4 || metacity &
-		xfdesktop &
-		xfce4-panel &
-	}
-} &
+dbus-launch --exit-with-session sh -c 'xfce4-session ; setsid sh -c "cd /proc/ ; for f in [0-9]* ; do [ \$f = \$\$ ] || kill -9 \$f ; done"' &
+
+sleep 3
 gimp &
 inkscape &
+
 wait
