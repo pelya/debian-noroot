353e21dcc59e8441d34c09e5702d42e996fc3af3
pelya <x.pelya.x@gmail.com>
2014-09-25 00:10:42 +0300

    Preliminary audio support

--- dist/start-gimp.sh
+++ dist/start-gimp.sh
@@ -3,6 +3,8 @@
 rm -f /var/run/dbus/pid
 dbus-daemon --system &
 sleep 1
+pulseaudio --start
+pulseaudio --load="module-simple-protocol-unix rate=44100 format=s16le channels=2 playback=true socket=/img/tmp/audio-out"
 dbus-launch --exit-with-session sh -c 'xfce4-session ; setsid sh -c "cd /proc/ ; for f in [0-9]* ; do [ \$f = \$\$ ] || kill -9 \$f ; done"' &
 
 sleep 3
