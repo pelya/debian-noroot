763eb41eb304b217189f9a982537dd643ef25f45
Sergii Pylypenko <x.pelya.x@gmail.com>
2014-10-07 22:16:38 +0300

    More fixes to audio

--- dist/startx.sh
+++ dist/startx.sh
@@ -5,6 +5,7 @@ fakeroot-tcp /usr/bin/env PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin
 
 sleep 1
 rm -f /tmp/audio-out
+mkfifo /tmp/audio-out
 pulseaudio --start
 dbus-launch --exit-with-session sh -c 'xfce4-session ; setsid sh -c "cd /proc/ ; for f in [0-9]* ; do [ \$f = \$\$ ] || kill -9 \$f ; done"' &
 
