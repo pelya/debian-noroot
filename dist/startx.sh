831cc121015ea18b10bc1967e5b71daa4b88a7e9
Sergii Pylypenko <x.pelya.x@gmail.com>
2014-10-07 20:58:23 +0300

    Fixed PulseAudio config, updated PRoot

--- dist/startx.sh
+++ dist/startx.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 rm -f /var/run/dbus/pid
-dbus-daemon --system &
+fakeroot-tcp /usr/bin/env PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin dbus-daemon --system
+
 sleep 1
+rm -f /tmp/audio-out
 pulseaudio --start
-pulseaudio --load="module-simple-protocol-unix rate=44100 format=s16le channels=2 playback=true socket=/img/tmp/audio-out"
 dbus-launch --exit-with-session sh -c 'xfce4-session ; setsid sh -c "cd /proc/ ; for f in [0-9]* ; do [ \$f = \$\$ ] || kill -9 \$f ; done"' &
 
 wait
