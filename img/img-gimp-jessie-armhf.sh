005468b9620bd5cb7015bacfbaaef89a6c8f04cb
pelya <x.pelya.x@gmail.com>
2015-03-22 22:26:36 +0200

    Scripts to generate Jessie GIMP images

---
+++ img/img-gimp-jessie-armhf.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+DIR=dist-gimp-jessie-armeabi-v7a/img
+sudo rm -r -f $DIR
+mkdir -p $DIR
+APT_CACHER=
+[ -e /etc/init.d/apt-cacher ] && APT_CACHER=/localhost:3142
+sudo qemu-debootstrap --arch=armhf --verbose \
+        --components=main,contrib,non-free \
+        --include=python-minimal,`cat img-gimp-wheezy.pkg` \
+        jessie $DIR http:/$APT_CACHER/ftp.ua.debian.org/debian/ \
+&& cat sources-jessie.list | sudo tee $DIR/etc/apt/sources.list > /dev/null \
+&& sudo ./prepare-img-proot.sh --update-packages $DIR
