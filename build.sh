2aefebd04049b5319ef6361321cac929512edc5d
Sergii Pylypenko <x.pelya.x@gmail.com>
2019-11-07 22:32:58 +0200

    Updating build script in progress

--- build.sh
+++ build.sh
@@ -2,61 +2,36 @@
 
 fail() { echo "Compilation failed!" ; exit 1; }
 
-false && { # ===== Do not compile libfakechroot and libfakedns, they are not used anymore =====
+ARCH_LIST="arm64-v8a x86_64 armeabi-v7a x86"
 
-cd fakechroot
-[ -e configure ] || ./autogen.sh || fail
-[ -e config.h ] || ./configure --host=arm-linux-gnueabihf --prefix=/usr || fail
-[ -e ../libfakechroot.so ] || {
-	make -j4 CFLAGS="-march=armv7-a -fpic" LDFLAGS="-march=armv7-a" V=1 && \
-	cp -f src/.libs/libfakechroot.so .. && \
-	arm-linux-gnueabihf-strip ../libfakechroot.so || fail
-} || fail
-cd ..
+for ARCH in $ARCH_LIST; do
 
-cd c-ares
-[ -e ares_config.h ] || LIBS=-Wl,--version-script=exports.txt \
-	./configure --enable-shared --host=arm-linux-gnueabihf --prefix=/usr || fail
-[ -e ../libfakedns.so ] || {
-	make -j4 CFLAGS="-march=armv7-a -fpic" LDFLAGS="-march=armv7-a" libcares.la && \
-	cp -f .libs/libcares.so ../libfakedns.so && \
-	arm-linux-gnueabihf-strip ../libfakedns.so || fail
-} || fail
-cd ..
+	export ARCH=$ARCH
 
-[ -e libfakedns.so ] || {
-	arm-linux-gnueabihf-gcc -march=armv7-a -shared -fpic fakedns/*.c -I c-ares c-ares/.libs/libcares.a -o libfakedns.so && \
-	arm-linux-gnueabihf-strip libfakedns.so || fail
-} || fail
+	mkdir -p dist-$ARCH
 
-} # ===== libfakechroot and libfakedns =====
+	[ -e dist-$ARCH/libdisableselinux.so ] || {
+		./setCrossEnvironment-$ARCH.sh sh -c ' \
+			$CC $CFLAGS $LDFLAGS -shared disableselinux/*.c -o dist-$ARCH/libdisableselinux.so && \
+			$STRIP dist-$ARCH/libdisableselinux.so' || fail
+	} || fail
 
-[ -e dist/libdisableselinux.so ] || {
-	arm-linux-gnueabihf-gcc -march=armv7-a -shared -fpic disableselinux/*.c -o dist/libdisableselinux.so && \
-	arm-linux-gnueabihf-strip dist/libdisableselinux.so || fail
-} || fail
+	[ -e dist-$ARCH/libandroid-shmem.so ] || {
 
-[ -e dist/libandroid-shmem.so ] || {
-	[ -e android-shmem/LICENSE ] || {
-		cd ..
-		git submodule update --init android/android-shmem || fail
-		cd $BUILDDIR
-	} || exit 1
-	[ -e android-shmem/libancillary/ancillary.h ] || {
 		cd android-shmem
-		git submodule update --init libancillary || fail
+		../setCrossEnvironment-$ARCH.sh sh -c ' \
+			$CC $CFLAGS $LDFLAGS -llog -shared -std=gnu99 *.c -I . -I libancillary \
+			-o ../dist-$ARCH/libandroid-shmem.so -Wl,--version-script=exports.txt && \
+			$STRIP ../dist-$ARCH/libandroid-shmem.so' || fail
 		cd ..
-	} || exit 1
+	} || fail
 
-	cd android-shmem
-	arm-linux-gnueabihf-gcc -march=armv7-a -shared -fpic -std=gnu99 -flto *.c -I . -I libancillary \
-		-o ../dist/libandroid-shmem.so -Wl,--version-script=exports.txt -lc -lpthread && \
-	arm-linux-gnueabihf-strip ../dist/libandroid-shmem.so || fail
-	cd ..
-} || fail
+	[ -e talloc-2.1.0.tar.gz ] || wget http://www.samba.org/ftp/talloc/talloc-2.1.0.tar.gz || fail
 
-[ -e libtalloc.a ] || {
-	[ -e talloc-2.1.0 ] || curl -L http://www.samba.org/ftp/talloc/talloc-2.1.0.tar.gz | tar xvz || fail
+	continue # BUILD SCRIPT REWORK IN PROGRESS
+
+[ -e libtalloc-$ARCH.a ] || {
+	[ -e talloc-2.1.0 ] || tar xvzf talloc-2.1.0.tar.gz || fail
 	cd talloc-2.1.0
 	make clean
 	env CC=arm-linux-gnueabihf-gcc CFLAGS="-flto -fpic" LD=arm-linux-gnueabihf-gcc LDFLAGS="-flto" ./configure build --cross-compile --cross-execute='qemu-arm-static /usr/arm-linux-gnueabihf/lib/ld-linux.so.3 --library-path /usr/arm-linux-gnueabihf/lib' || fail
@@ -122,3 +97,5 @@ CFLAGSx86="-march=i686 -mtune=atom -mstackrealign -msse3 -mfpmath=sse -m32 -flto
 	cd ../..
 	strip dist-x86/proot
 } || fail
+
+done
