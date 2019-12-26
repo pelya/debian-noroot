9fba74c60f9abce191cdf2c9b27b8315600b1c76
Sergii Pylypenko <x.pelya.x@gmail.com>
2019-12-26 23:09:07 +0200

    Updated proot start script

--- dist/proot.sh
+++ dist/proot.sh
@@ -8,11 +8,6 @@ case x$SDCARD in x ) export SDCARD=$EXTERNAL_STORAGE;; esac
 
 SDCARD=`../busybox realpath $SDCARD`
 STORAGE="-b $SDCARD"
-for F in 0 1 2 3 4 5 6 7 8 9; do
-	DIR=`eval echo \\$UNSECURE_STORAGE_DIR_$F`
-	case x$DIR in x ) echo > /dev/null;; * ) STORAGE="$STORAGE -b $DIR" ; mkdir -p $DIR ;; esac
-done
-echo "STORAGE $STORAGE"
 
 export HOME=/home/$USER
 export SHELL=/bin/bash
@@ -25,4 +20,4 @@ export "LD_PRELOAD=/libdisableselinux.so /libandroid-shmem.so"
 export PROOT_TMPDIR=`pwd`/tmp
 export PROOT_TMP_DIR=$PROOT_TMPDIR
 export TZ="`getprop persist.sys.timezone`"
-./proot -r `pwd` -w / -b /dev -b /proc -b /sys -b /system $STORAGE "$@"
+../usr/bin/proot -r `pwd` -w / -b /dev -b /proc -b /sys -b /system -b /data -b /mnt -b /storage /b odm /b oem /b vendor $STORAGE "$@"
