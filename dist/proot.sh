43822682defcfbd771dea4b96aaadc38747d750f
Sergii Pylypenko <x.pelya.x@gmail.com>
2019-12-31 21:03:06 +0200

    Fixed proot script

--- dist/proot.sh
+++ dist/proot.sh
@@ -12,11 +12,6 @@ STORAGE="-b $SDCARD"
 export HOME=/home/$USER
 export SHELL=/bin/bash
 export LD_LIBRARY_PATH=
-# Java doesn't work in PRoot when started from /usr/bin/java symlink, so we have to put a path to java binary into PATH, and Java 6 fails on Samsung devices
-#JAVA_PATH=/usr/lib/jvm/default-java/jre/bin:/usr/lib/jvm/default-java/bin
-#ls usr/lib/jvm/java-7-openjdk-*/bin > /dev/null 2>&1 && JAVA_PATH=/`echo usr/lib/jvm/java-7-openjdk-*/jre/bin`:/`echo usr/lib/jvm/java-7-openjdk-*/bin`
-export PATH=/usr/local/sbin:/usr/local/bin:$JAVA_PATH:/usr/sbin:/usr/bin:/sbin:/bin
-export "LD_PRELOAD=/libdisableselinux.so /libandroid-shmem.so"
 export TZ="`getprop persist.sys.timezone`"
 export PROOT_TMPDIR=`pwd`/tmp
 export PROOT_TMP_DIR=$PROOT_TMPDIR
--- dist/startx.sh
+++ dist/startx.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+# Java doesn't work in PRoot when started from /usr/bin/java symlink, so we have to put a path to java binary into PATH, and Java 6 fails on Samsung devices
+#JAVA_PATH=/usr/lib/jvm/default-java/jre/bin:/usr/lib/jvm/default-java/bin
+#ls usr/lib/jvm/java-7-openjdk-*/bin > /dev/null 2>&1 && JAVA_PATH=/`echo usr/lib/jvm/java-7-openjdk-*/jre/bin`:/`echo usr/lib/jvm/java-7-openjdk-*/bin`
+#export PATH=/usr/local/sbin:/usr/local/bin:$JAVA_PATH:/usr/sbin:/usr/bin:/sbin:/bin
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+export "LD_PRELOAD=/libdisableselinux.so /libandroid-shmem.so"
+
 rm -f /var/run/dbus/pid
 fakeroot-tcp /usr/bin/env PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin dbus-daemon --system
 
