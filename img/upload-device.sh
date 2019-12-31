dae2dca3f8f435acbd94ddb3cb275e9857178eb4
Sergii Pylypenko <x.pelya.x@gmail.com>
2019-12-31 19:05:47 +0200

    Scripts to upload image to device

---
+++ img/upload-device.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+adb push dist-debian-buster-arm64-v8a.tar.xz /sdcard/Android/obb/com.cuntubuntu/main.191224.com.cuntubuntu.obb
---
+++ img/upload-emulator.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+adb push dist-debian-buster-x86_64.tar.xz /sdcard/Android/obb/com.cuntubuntu/main.191224.com.cuntubuntu.obb
