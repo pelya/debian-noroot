029f53096f76b8ade53b380b6001df2e08d9b925
Sergii Pylypenko <x.pelya.x@gmail.com>
2020-01-02 21:46:37 +0200

    Updated proot script

--- dist/proot.sh
+++ dist/proot.sh
@@ -2,7 +2,8 @@
 
 case x$SDCARD in x ) SDCARD=/sdcard;; esac
 
-case x$SECURE_STORAGE_DIR in x ) echo > /dev/null;; * ) cd $SECURE_STORAGE_DIR/img;; esac
+case x$SECURE_STORAGE_DIR in x ) export SECURE_STORAGE_DIR=/data/data/com.cuntubuntu/files ;; esac
+cd $SECURE_STORAGE_DIR/img
 
 case x$SDCARD in x ) export SDCARD=$EXTERNAL_STORAGE;; esac
 
@@ -18,4 +19,8 @@ export PROOT_TMPDIR=`pwd`/tmp
 export PROOT_TMP_DIR=$PROOT_TMPDIR
 export PROOT_LOADER=$SECURE_STORAGE_DIR/usr/bin/loader
 export PROOT_LOADER_32=$SECURE_STORAGE_DIR/usr/bin/loader32
+
+ls -l $LD_PRELOAD
+echo "Launching proot with arguments:" "$@"
+
 ../usr/bin/proot -r `pwd` -w / -b /dev -b /proc -b /sys -b /system -b /data -b /mnt -b /storage -b /odm -b /oem -b /vendor $STORAGE "$@"
